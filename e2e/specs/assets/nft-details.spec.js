c2da74e97429605ed517ca02b89cf20c99dc422a
Curtis <Curtis.David7@gmail.com>
2024-09-27 16:55:42 -0400

    temp tagging smoke core for failing assets test

--- e2e/specs/assets/nft-details.spec.js
+++ e2e/specs/assets/nft-details.spec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { SmokeAssets } from '../../tags';
+import { SmokeCore } from '../../tags';
 import TestHelpers from '../../helpers';
 import { loginToApp } from '../../viewHelper';
 import FixtureBuilder from '../../fixtures/fixture-builder';
@@ -14,59 +14,56 @@ import AddCustomTokenView from '../../pages/AddCustomTokenView';
 import Assertions from '../../utils/Assertions';
 import enContent from '../../../locales/languages/en.json';
 
-describe('NFT Details page'),
-  () => {
-    const NFT_CONTRACT = SMART_CONTRACTS.NFTS;
-    const TEST_DAPP_CONTRACT = 'TestDappNFTs';
-    beforeAll(async () => {
-      jest.setTimeout(170000);
-      await TestHelpers.reverseServerPort();
-    });
+describe(SmokeCore('NFT Details page'), () => {
+  const NFT_CONTRACT = SMART_CONTRACTS.NFTS;
+  const TEST_DAPP_CONTRACT = 'TestDappNFTs';
+  beforeAll(async () => {
+    jest.setTimeout(170000);
+    await TestHelpers.reverseServerPort();
+  });
 
-    it('show nft details', async () => {
-      await withFixtures(
-        {
-          dapp: true,
-          fixture: new FixtureBuilder()
-            .withGanacheNetwork()
-            .withPermissionControllerConnectedToTestDapp()
-            .build(),
-          restartDevice: true,
-          ganacheOptions: defaultGanacheOptions,
-          smartContract: NFT_CONTRACT,
-        },
-        async ({ contractRegistry }) => {
-          const nftsAddress = await contractRegistry.getContractAddress(
-            NFT_CONTRACT,
-          );
+  it('show nft details', async () => {
+    await withFixtures(
+      {
+        dapp: true,
+        fixture: new FixtureBuilder()
+          .withGanacheNetwork()
+          .withPermissionControllerConnectedToTestDapp()
+          .build(),
+        restartDevice: true,
+        ganacheOptions: defaultGanacheOptions,
+        smartContract: NFT_CONTRACT,
+      },
+      async ({ contractRegistry }) => {
+        const nftsAddress = await contractRegistry.getContractAddress(
+          NFT_CONTRACT,
+        );
 
-          await loginToApp();
+        await loginToApp();
 
-          await WalletView.tapNftTab();
-          await WalletView.scrollDownOnNFTsTab();
-          // Tap on the add collectibles button
-          await WalletView.tapImportNFTButton();
-          await AddCustomTokenView.isVisible();
-          await AddCustomTokenView.typeInNFTAddress(nftsAddress);
-          await AddCustomTokenView.typeInNFTIdentifier('1');
+        await WalletView.tapNftTab();
+        await WalletView.scrollDownOnNFTsTab();
+        // Tap on the add collectibles button
+        await WalletView.tapImportNFTButton();
+        await AddCustomTokenView.isVisible();
+        await AddCustomTokenView.typeInNFTAddress(nftsAddress);
+        await AddCustomTokenView.typeInNFTIdentifier('1');
 
-          await Assertions.checkIfVisible(WalletView.container);
-          // Wait for asset to load
-          await Assertions.checkIfVisible(
-            WalletView.nftInWallet(TEST_DAPP_CONTRACT),
-          );
-          await WalletView.tapOnNftName();
+        await Assertions.checkIfVisible(WalletView.container);
+        // Wait for asset to load
+        await Assertions.checkIfVisible(
+          WalletView.nftInWallet(TEST_DAPP_CONTRACT),
+        );
+        await WalletView.tapOnNftName();
 
-          await Assertions.checkIfTextIsDisplayed(
-            enContent.nft_details.token_id,
-          );
-          await Assertions.checkIfTextIsDisplayed(
-            enContent.nft_details.contract_address,
-          );
-          await Assertions.checkIfTextIsDisplayed(
-            enContent.nft_details.token_standard,
-          );
-        },
-      );
-    });
-  };
+        await Assertions.checkIfTextIsDisplayed(enContent.nft_details.token_id);
+        await Assertions.checkIfTextIsDisplayed(
+          enContent.nft_details.contract_address,
+        );
+        await Assertions.checkIfTextIsDisplayed(
+          enContent.nft_details.token_standard,
+        );
+      },
+    );
+  });
+});
