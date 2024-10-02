ed02eb3563e45427940a538bfe2f4cab68bf9936
Curtis <Curtis.David7@gmail.com>
2024-10-02 19:14:59 -0400

    attempt to fix crasher with nfts

--- app/components/UI/CollectibleContractElement/index.js
+++ app/components/UI/CollectibleContractElement/index.js
@@ -254,13 +254,19 @@ function CollectibleContractElement({
       </TouchableOpacity>
       {collectiblesVisible && (
         <View style={styles.grid}>
-          {/* {collectiblesGrid.map((row, i) => (
-            <View key={i} style={styles.collectiblesRowContainer}>
-              {row.map((collectible, index) =>
-                renderCollectible({ ...collectible, logo: asset.logo }, index),
+          {collectiblesGrid.map((row, rowIndex) => (
+            <View
+              key={`row-${rowIndex}`}
+              style={styles.collectiblesRowContainer}
+            >
+              {row.map((collectible, colIndex) =>
+                renderCollectible(
+                  { ...collectible, logo: asset.logo },
+                  `collectible-${rowIndex}-${colIndex}`,
+                ),
               )}
             </View>
-          ))} */}
+          ))}
         </View>
       )}
       <ActionSheet
