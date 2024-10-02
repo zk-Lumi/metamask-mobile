24856a8907a78f6bf4c43a55aa6e38cebe775df4
Curtis <Curtis.David7@gmail.com>
2024-10-02 19:43:43 -0400

    Revert "attempt to fix crasher with nfts"
    This reverts commit ed02eb3563e45427940a538bfe2f4cab68bf9936.

--- app/components/UI/CollectibleContractElement/index.js
+++ app/components/UI/CollectibleContractElement/index.js
@@ -254,19 +254,13 @@ function CollectibleContractElement({
       </TouchableOpacity>
       {collectiblesVisible && (
         <View style={styles.grid}>
-          {collectiblesGrid.map((row, rowIndex) => (
-            <View
-              key={`row-${rowIndex}`}
-              style={styles.collectiblesRowContainer}
-            >
-              {row.map((collectible, colIndex) =>
-                renderCollectible(
-                  { ...collectible, logo: asset.logo },
-                  `collectible-${rowIndex}-${colIndex}`,
-                ),
+          {/* {collectiblesGrid.map((row, i) => (
+            <View key={i} style={styles.collectiblesRowContainer}>
+              {row.map((collectible, index) =>
+                renderCollectible({ ...collectible, logo: asset.logo }, index),
               )}
             </View>
-          ))}
+          ))} */}
         </View>
       )}
       <ActionSheet
