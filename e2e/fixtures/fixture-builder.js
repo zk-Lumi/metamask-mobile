88eef82b052a171d1aeea73c8436d56021d65d57
Davide Brocchetto <davide.brocchetto@consensys.net>
2024-10-24 21:45:16 +0200

    Removed network tests

--- e2e/fixtures/fixture-builder.js
+++ e2e/fixtures/fixture-builder.js
@@ -704,12 +704,12 @@ class FixtureBuilder {
       chainId: providerConfig.chainId,
       rpcEndpoints: [
         {
-         networkClientId: newNetworkClientId,
-         url: providerConfig.rpcUrl,
-         type: providerConfig.type,
-         name: providerConfig.nickname,
+          networkClientId: newNetworkClientId,
+          url: providerConfig.rpcUrl,
+          type: providerConfig.type,
+          name: providerConfig.nickname,
         },
-     ],
+      ],
       defaultRpcEndpointIndex: 0,
       blockExplorerUrls: [],
       name: providerConfig.nickname,
@@ -717,7 +717,8 @@ class FixtureBuilder {
     };
 
     // Add the new network configuration to the object
-    networkController.networkConfigurationsByChainId[providerConfig.chainId] = networkConfig;
+    networkController.networkConfigurationsByChainId[providerConfig.chainId] =
+      networkConfig;
 
     // Update selectedNetworkClientId to the new network client ID
     networkController.selectedNetworkClientId = newNetworkClientId;
